c8e577422ed35432c53d8860f4fa436210296972
Glitchscode <sanuthabdulmumin2@gmail.com>
2024-02-28 03:07:27 +0100

    Updated shell

--- file_loc.c
+++ file_loc.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
-  * startsWithForwardSlash - Checks if file starts with "/"
-  * @str: The filename to be checked
-  *
-  * Return: 0 if yes and 1 if NO
-  */
+ * startsWithForwardSlash - Checks if file starts with "/"
+ * @str: The filename to be checked
+ *
+ * Return: 0 if yes and 1 if NO
+ */
 
 int startsWithForwardSlash(const char *str)
 {
-        if (str != NULL || str[0] == '/')
-                return (1);
+	if (str != NULL || str[0] == '/')
+		return (1);
 
-        return (0);
+	return (0);
 }
 /**
  * get_file_loc - Get the executable path of file
@@ -23,58 +23,58 @@ int startsWithForwardSlash(const char *str)
 
 char *get_file_loc(char *path, char *file_name)
 {
-        char *path_copy, *token;
-        struct stat file_path;
-        char *path_buffer = NULL;
+	char *path_copy, *token;
+	struct stat file_path;
+	char *path_buffer = NULL;
 
-        path_copy = strdup(path);
-        token = strtok(path_copy, ":");
+	path_copy = strdup(path);
+	token = strtok(path_copy, ":");
 
-        while (token)
-        {
-                if (path_buffer)
-                {
-                        free(path_buffer);
-                        path_buffer = NULL;
-                }
-                path_buffer = malloc(strlen(token) + strlen(file_name) + 2);
-                if (!path_buffer)
-                {
-                        perror("Error: malloc failed");
-                        exit(EXIT_FAILURE);
-                }
-                strcpy(path_buffer, token);
-                strcat(path_buffer, "/");
-                strcat(path_buffer, file_name);
-                strcat(path_buffer, "\0");
+	while (token)
+	{
+		if (path_buffer)
+		{
+			free(path_buffer);
+			path_buffer = NULL;
+		}
+		path_buffer = malloc(strlen(token) + strlen(file_name) + 2);
+		if (!path_buffer)
+		{
+			perror("Error: malloc failed");
+			exit(EXIT_FAILURE);
+		}
+		strcpy(path_buffer, token);
+		strcat(path_buffer, "/");
+		strcat(path_buffer, file_name);
+		strcat(path_buffer, "\0");
 
-                if (stat(path_buffer, &file_path) == 0 && access(path_buffer, X_OK) == 0)
-                {
-                        free(path_copy);
-                        return (path_buffer);
-                }
-                token = strtok(NULL, ":");
-        }
-        free(path_copy);
-        if (path_buffer)
-                free(path_buffer);
-        return (NULL);
+		if (stat(path_buffer, &file_path) == 0 && access(path_buffer, X_OK) == 0)
+		{
+			free(path_copy);
+			return (path_buffer);
+		}
+		token = strtok(NULL, ":");
+	}
+	free(path_copy);
+	if (path_buffer)
+		free(path_buffer);
+	return (NULL);
 }
 
 /**
-  * get_file_path - Get's the full path of the file
-  * @file_name: Argument name
-  *
-  * Return: The full path argument to the file
-  */
+ * get_file_path - Get's the full path of the file
+ * @file_name: Argument name
+ *
+ * Return: The full path argument to the file
+ */
 char *get_file_path(char *file_name)
 {
 	char *path = getenv("PATH");
 	char *full_path;
 
 	if (startsWithForwardSlash(file_name) &&
-                        access(file_name, X_OK) == 0)
-                return (strdup(file_name));
+	    access(file_name, X_OK) == 0)
+		return (strdup(file_name));
 
 	if (!path)
 	{
--- main.c
+++ main.c
@@ -10,66 +10,65 @@
 
 int main(int argc, char **argv)
 {
-        (void)argc;
-        char *buf = NULL, *token;
-        size_t count = 0;
-        ssize_t nread;
-        pid_t child_pid;
-        int i, status;
-        /*char **array;*/
+	(void)argc;
+	char *buf = NULL, *token;
+	size_t count = 0;
+	ssize_t nread;
+	pid_t child_pid;
+	int i, status;
 	char *path;
 
-        while (1)
-        {
-	Here:
+	while (1)
+	{
+Here:
 		if (isatty(STDIN_FILENO))
 			write(STDOUT_FILENO, "$ ", 2);
 
-                nread = getline(&buf, &count, stdin);
+		nread = getline(&buf, &count, stdin);
 
-                if (nread ==  -1)
-                {
-                        exit(1);
-                }
+		if (nread ==  -1)
+		{
+			exit(1);
+		}
 
 		if (nread == 1)
 			goto Here;
 
-                token = strtok(buf, " \n");
+		token = strtok(buf, " \n");
 
-                argv = malloc(sizeof(char*) * 1024);
-                i = 0;
+		argv = malloc(sizeof(char *) * 1024);
+		i = 0;
 
-                while (token != NULL)
-                {
-                        argv[i] = token;
-                        token = strtok(NULL, " \n");
-                        i++;
-                }
+		while (token != NULL)
+		{
+			argv[i] = token;
+			token = strtok(NULL, " \n");
+			i++;
+		}
 
-                argv[i] = NULL;
+		argv[i] = NULL;
 		path = get_file_path(argv[0]);
-                child_pid = fork();
+		child_pid = fork();
 
-                if (child_pid == -1)
-                {
-                        perror("Failed to create.");
-                        exit (41);
-                }
+		if (child_pid == -1)
+		{
+			perror("Failed to create.");
+			exit(41);
+		}
 
-                if (child_pid == 0)
-                {
-                        if (execve(path, argv, NULL) == -1)
-                        {
-                                exit(97);
-                        }
-                }
-                else
-                {
-                        wait(&status);
-                }
-        }
+		if (child_pid == 0)
+		{
+			if (execve(path, argv, NULL) == -1)
+			{
+				exit(97);
+			}
+		}
+		else
+		{
+			wait(&status);
+		}
+	}
 	free(path);
-        free(buf);
-        return (0);
+	free(buf);
+	return (0);
 }
